5f200a9810b46e21f37b24faf31b525760db155e
Cun Song <96717458+cun-song@users.noreply.github.com>
2024-08-27 12:07:48 +0700

    Update PrintInvoice.js

--- src/utils/PrintInvoice.js
+++ src/utils/PrintInvoice.js
@@ -66,8 +66,8 @@ const Table = ({ data }) => {
   const bonusData = data?.product?.filter((product) => product?.price === 0);
   const nbl = nonBonusData?.length;
   return (
-    <div style={{ padding: "20px",paddingTop:"5px", width: "8in", height: "5.5in", boxSizing: "border-box", position: "relative" }}>
-      <div style={{ position: "absolute", top: 10, left: 20 }}>
+    <div style={{ padding: "20px",paddingTop:"8px", width: "8in", height: "5.5in", boxSizing: "border-box", position: "relative" }}>
+      <div style={{ position: "absolute", top: 13, left: 20 }}>
         <img src={elangVector} style={{ height: "40px", width: "auto" }} />
       </div>
       <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginBottom: "10px", paddingBottom: "5px", borderBottom: "2px double black" }}>
